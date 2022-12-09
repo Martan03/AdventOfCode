b9fb1dc4fa01f81493bf7f95c3ff95847cf02c25
Martan03 <martinslezak03@gmail.com>
2022-12-10 00:11:19 +0100

    Add 8 day 2 part

--- 2022/08/src/main.cpp
+++ 2022/08/src/main.cpp
@@ -3,13 +3,20 @@
 
 int first_part();
 
+int second_part();
+
 std::vector<std::vector<int>> get_trees();
 
 bool is_hidden(std::vector<std::vector<int>> trees, int x, int y);
 
+int get_scenic_score(std::vector<std::vector<int>> trees, int x, int y);
+
 int main()
 {
-    std::cout << "First part: " << first_part() << std::endl;
+    //std::cout << "First part: " << first_part() << std::endl;
+    std::cout << "Second part: " << second_part() << std::endl;
+
+    return 0;
 }
 
 int first_part()
@@ -23,6 +30,19 @@ int first_part()
             visible += !is_hidden(trees, j, i);
     
     return visible;
+}
+
+int second_part()
+{
+    std::vector<std::vector<int>> trees = get_trees();
+
+    int score = 0;
+
+    for (int i = 0; i < trees.size(); ++i)
+        for (int j = 0; j < trees[i].size(); ++j)
+            score = std::max(score, get_scenic_score(trees, j, i));
+            
+    return score;
 }
 
 std::vector<std::vector<int>> get_trees()
@@ -47,9 +67,22 @@ bool is_hidden(std::vector<std::vector<int>> trees, int x, int y)
     int yUp = 0, yDown = trees.front().size() - 1;
 
     while (trees[y][xLeft] < trees[y][x] && xLeft <= x) ++xLeft;
-    while(trees[y][xRight] < trees[y][x] && xRight >= x) --xRight;
-    while(trees[yUp][x] < trees[y][x] && yUp <= y) ++yUp;
-    while(trees[yDown][x] < trees[y][x] && yDown >= y) --yDown;
+    while (trees[y][xRight] < trees[y][x] && xRight >= x) --xRight;
+    while (trees[yUp][x] < trees[y][x] && yUp <= y) ++yUp;
+    while (trees[yDown][x] < trees[y][x] && yDown >= y) --yDown;
 
     return xLeft < x && xRight > x && yUp < y && yDown > y;
+}
+
+int get_scenic_score(std::vector<std::vector<int>> trees, int x, int y)
+{
+    int xLeft = x - 1, xRight = x + 1;
+    int yUp = y - 1, yDown = y + 1;
+    
+    while (xLeft > 0 && trees[y][xLeft] < trees[y][x]) --xLeft;
+    while (xRight < trees.front().size() - 1 && trees[y][xRight] < trees[y][x]) ++xRight;
+    while (yUp > 0 && trees[yUp][x] < trees[y][x]) --yUp;
+    while (yDown < trees.size() - 1 && trees[yDown][x] < trees[y][x]) ++yDown;
+
+    return (x - xLeft) * (xRight - x) * (y - yUp) * (yDown - y);
 }
