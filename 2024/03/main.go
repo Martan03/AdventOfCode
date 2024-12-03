1235157cedaea43259c1176c879fcc9922b6924c
Martan03 <martinslezak03@gmail.com>
2024-12-03 17:21:16 +0100

    Add solution for second part of day 3

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -3,40 +3,62 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strconv"
 )
 
+var pattern = regexp.MustCompile(`mul\((\d+),(\d+)\)|don't\(\)|do\(\)`)
+
 func main() {
-	part1()
+	totalCollect, totalSkip, err := multiple_sum("input.txt")
+	if err != nil {
+		log.Fatal("Error:", err)
+		return
+	}
+
+	fmt.Println("Part 1:", totalCollect+totalSkip)
+	fmt.Println("Part 2:", totalCollect)
 }
 
-func part1() {
-	file, err := os.Open("input.txt")
+func multiple_sum(filename string) (int, int, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return 0, 0, err
 	}
 	defer file.Close()
 
-	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	var totalCollect, totalSkip int
+	collecting := true
+
 	scanner := bufio.NewScanner(file)
-
-	total := 0
 	for scanner.Scan() {
-		line := scanner.Text()
-
-		matches := re.FindAllStringSubmatch(line, -1)
-		for _, match := range matches {
-			num1, err1 := strconv.Atoi(match[1])
-			num2, err2 := strconv.Atoi(match[2])
-			if err1 != nil || err2 != nil {
-				continue
-			}
-			total += num1 * num2
-		}
+		processLine(scanner.Text(), &totalCollect, &totalSkip, &collecting)
 	}
 
-	fmt.Println("Total:", total)
+	if err := scanner.Err(); err != nil {
+		return 0, 0, err
+	}
+	return totalCollect, totalSkip, nil
+}
+
+func processLine(line string, totalCollect, totalSkip *int, collecting *bool) {
+	for _, match := range pattern.FindAllStringSubmatch(line, -1) {
+		switch match[0] {
+		case "don't()":
+			*collecting = false
+		case "do()":
+			*collecting = true
+		default:
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+
+			if *collecting {
+				*totalCollect += num1 * num2
+			} else {
+				*totalSkip += num1 * num2
+			}
+		}
+	}
 }
